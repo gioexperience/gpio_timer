2d4d86f42b79f7ac00fcaaaf762abf6b21455fd6
GioExperience <50233919+gioexperience@users.noreply.github.com>
2019-09-05 13:54:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ apt-get install wiringpi php -y
 chmod +x ~/gpio_timer/update.sh
 
 echo "installing serc..."
-cp ./source/gpio_timer /usr/local/bin/gpio_timer
+cp ~/gpio_timer/source/gpio_timer /usr/local/bin/gpio_timer
 chmod +x /usr/local/bin/gpio_timer
 
 echo "Finish"
